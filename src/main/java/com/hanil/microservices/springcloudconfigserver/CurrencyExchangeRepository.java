6252c3aef7d2abded10837b85b3fd5de0d3153e0
Hanil Trivedi <trivedihanil@gmail.com>
2021-09-05 16:00:36 +0530

    Create a JPA Repository and then call from controller instead of hardcoded values.and the url will be http://localhost:8000/currency-exchange/from/USD/to/INR
    Signed-off-by: Hanil Trivedi <trivedihanil@gmail.com>

--- src/main/java/com/hanil/microservices/springcloudconfigserver/CurrencyExchangeRepository.java
+++ src/main/java/com/hanil/microservices/springcloudconfigserver/CurrencyExchangeRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
 
+	CurrencyExchange findByFromAndTo(String from , String to);
 	
 }
