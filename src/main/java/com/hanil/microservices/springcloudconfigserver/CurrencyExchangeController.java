cc5a27ed418a71f53db1719bf11df93f5d53a9a1
Hanil Trivedi <trivedihanil@gmail.com>
2021-09-23 22:18:57 +0530

    added Zipkin distributed tracing for all MS and logging at controller

--- src/main/java/com/hanil/microservices/springcloudconfigserver/CurrencyExchangeController.java
+++ src/main/java/com/hanil/microservices/springcloudconfigserver/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.hanil.microservices.springcloudconfigserver;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	CurrencyExchangeRepository repository;
 	
@@ -21,6 +25,9 @@ public class CurrencyExchangeController {
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {} ", from , to);
+		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
